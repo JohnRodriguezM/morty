5da7799b0dc0832306d8d1211dfbdaeea7634ea0
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-08-11 20:45:33 -0500

    a

--- src/js/manejoApi/restApiConsume.mjs
+++ src/js/manejoApi/restApiConsume.mjs
@@ -6,6 +6,10 @@ import { URL_OBJ } from './urls.mjs'
 import { DOM_ELEMENTS } from '../DomElements/getDomElements.mjs'
 
 
+const elementos = []
+
+
+
 export const getAllCharacters = async () => {
   const { GET_ALL_CHARACTERS } = URL_OBJ;
   const { sectionCharacter } = DOM_ELEMENTS;
@@ -24,7 +28,10 @@ export const getAllCharacters = async () => {
           status: el.status,
           species: el.species,
           image: el.image,
-
+          origin: el.origin.name,
+          location: el.location.name,
+          episode: el.episode.length,
+          created: el.created
         }
       }
     )
@@ -35,10 +42,21 @@ export const getAllCharacters = async () => {
       divContainerCharacter.classList.add('container-character');
 
 
+      let pOrigin = document.createElement('p');
+      pOrigin.id = el.origin;
+      let episode = document.createElement('p');
+      episode.id = el.episode;
+      let pLocation = document.createElement('p');
+      pLocation.id = el.location;
+      let pCreated = document.createElement('p');
+      pCreated.id = el.created;
+
+
+
       let h3Character = document.createElement('h3')
       let genderCharacter = document.createElement('p')
       let imgCharacter = document.createElement('img')
-      imgCharacter.setAttribute('id',el.name)
+      imgCharacter.setAttribute('id', el.name)
 
       h3Character.innerText = `${el.name}`
       genderCharacter.innerHTML = el.gender;
@@ -54,11 +72,24 @@ export const getAllCharacters = async () => {
     characters.forEach(el => {
       el.addEventListener('click', (event) => {
         event.preventDefault()
-        const id = event.target.id
-        console.log(id)
+        const element = {
+
+          name: el.querySelector('h3').innerText,
+          gender: el.querySelector('p').innerText,
+         /* created: el.querySelector('p:nth-child(3)').id,
+          origin: el.querySelector('p:nth-child(4)').id,
+          location: el.querySelector('p:nth-child(5)').id,
+          episode: el.querySelector('p:nth-child(6)').id,*/
+          image: el.querySelector('img').src
+        }
+        elementos.push(element)
+        console.log(element);
+        console.log(elementos);
+        return element;
       })
     }
     )
+    //!kjfnjafohaouñfhnwoñufhnoñsianvoasnvños
     console.log(characters);
 
   }
